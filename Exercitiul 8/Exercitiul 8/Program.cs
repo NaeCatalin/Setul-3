4366c1776f890d29a921620ccb128057c2d08adf
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 15:40:29 +0200

    Add files via upload

---
+++ Exercitiul 8/Exercitiul 8/Program.cs
@@ -0,0 +1,39 @@
+﻿//Rotire.
+//Se da un vector cu n elemente.
+//Rotiti elementele vectorului cu o pozitie spre stanga.
+//Prin rotire spre stanga primul element devine ultimul, al doilea devine primul etc.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] victor = new int[n];
+
+        Console.WriteLine("Introduceti elementele:");
+        for (int i = 0;i < n; i++)
+        {
+            victor[i] = int.Parse(Console.ReadLine());
+
+        }
+     int firstElement = victor[0];
+        for (int i = 0; i < n - 1; i++)
+        {
+            victor[i] = victor[i + 1];
+        }
+        victor[n - 1] = firstElement;
+
+        Console.WriteLine("Vectorul rotit spre stanga:");
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(victor[i] + " ");
+        }
+
+
+    }
+}

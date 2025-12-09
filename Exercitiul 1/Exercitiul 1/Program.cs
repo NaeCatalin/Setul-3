a5c545dafb6a5a41cc22f559fe721ced42365f6a
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 15:34:25 +0200

    Add files via upload

---
+++ Exercitiul 1/Exercitiul 1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Calculati suma elementelor unui vector de n numere citite de la tastatura.
+//Rezultatul se va afisa pe ecran.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduceti numarul de elemente:");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] v = new int[n];
+        int suma = 0;
+
+        Console.WriteLine("Introduceti cele " + n + " numere:");
+        for (int i = 0; i < n; i++)
+        {
+            v[i] = int.Parse(Console.ReadLine());
+            suma += v[i];
+        }
+        Console.WriteLine("Suma elementelor vectorului este: " + suma);
+        
+    }
+}

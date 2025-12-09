9b6855a8068c2eef0c25a0f8fe56e77a169884a0
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 15:39:59 +0200

    Add files via upload

---
+++ Exercitiul 7/Exercitiul 7/Program.cs
@@ -0,0 +1,36 @@
+﻿//Reverse. Se da un vector nu n elemente.
+//Se cere sa se inverseze ordinea elementelor din vector.
+//Prin inversare se intelege ca primul element devine ultimul,
+//al doilea devine penultimul etc.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+
+        int [] vector = new int[n];
+
+        Console.WriteLine("Introdu elementele");
+        for (int i = 0;i < n; i++)
+        {
+            vector[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < n / 2; i++)
+        {
+            int aux = vector[i];
+            vector[i] = vector[n - i - 1];
+            vector[n - i - 1] = aux;
+        }
+        Console.WriteLine("Vectorul selectat: ");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(vector[i]);
+        }
+
+    }
+}

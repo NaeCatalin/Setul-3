671f842b6e05b538bb989db9827cb16a2c8eafbe
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 15:43:31 +0200

    Add files via upload

---
+++ Exercitiul 15/Exercitiul 15/Program.cs
@@ -0,0 +1,50 @@
+﻿//Modificati un vector prin eliminarea elementelor care se repeta,fara a folosi un alt vector. 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] v = new int[n];
+
+        Console.WriteLine("Introdu elementele vectorului:");
+        for (int i = 0; i < n; i++)
+        {
+            v[i] = int.Parse(Console.ReadLine());
+        }
+
+        int k = 0; 
+
+        for (int i = 0; i < n; i++)
+        {
+            bool apare = false;
+
+           
+            for (int j = 0; j < k; j++)
+            {
+                if (v[i] == v[j])
+                {
+                    apare = true;
+                    break;
+                }
+            }
+
+            
+            if (!apare)
+            {
+                v[k] = v[i];
+                k++;
+            }
+        }
+
+        Console.WriteLine("Vectorul fara elemente duplicate este:");
+        for (int i = 0; i < k; i++)
+        {
+            Console.Write(v[i] + " ");
+        }
+    }
+}

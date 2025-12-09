03b81e3623abe50e39399613b312772fdceeb156
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 15:42:38 +0200

    Add files via upload

---
+++ Exercitiul 13/Exercitiul 13/Program.cs
@@ -0,0 +1,43 @@
+﻿//Sortare prin insertie. Implementati algoritmul de sortare <Insertion Sort>. 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("n =");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] vector = new int[n];
+
+        Console.WriteLine("Introduceti elementele vectorului:");
+        for (int i = 0; i < n; i++)
+        {
+            vector[i] = int.Parse(Console.ReadLine());
+        }
+        for (int i = 1; i < n; i++)
+        {
+            int key = vector[i];
+            int j = i - 1;
+
+            vector[j + 1] = vector[j];
+
+            while (j >= 0 && vector[j] > key)
+            {
+                vector[j + 1] = vector[j];
+                j--;
+            }
+        vector[j + 1] = key;
+       
+
+        }
+        Console.WriteLine("Vector sortat este:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(vector[i] + " ");
+        }
+
+
+    }
+}
